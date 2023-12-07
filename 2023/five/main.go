eebcf27a62c09683a697a586b49c5c812fa406f4
Shaun Adams <s.adams@f5.com>
2023-12-07 16:46:13 +0000

    Optimized range based solution for part 2

--- 2023/five/main.go
+++ 2023/five/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -18,14 +19,47 @@ var (
 type CustomRange struct {
 	outputMin int
 	inputMin  int
-	rangeSize int
+	inputMax  int // incl
 }
 
-func (c *CustomRange) getOutput(input int) int {
-	if input >= c.inputMin && input < c.inputMin+c.rangeSize {
-		return c.outputMin + (input - c.inputMin)
+type Span struct {
+	min int
+	max int // both incl
+}
+
+func calcSpans(span Span, mMap CustomMap) []Span {
+	res := make([]Span, 0)
+
+	newSpan := span
+	for idx, rng := range mMap.ranges {
+		if newSpan.min > rng.inputMax {
+			if idx == len(mMap.ranges)-1 {
+				res = append(res, newSpan)
+			}
+			continue
+		}
+
+		if newSpan.min > rng.inputMax {
+			continue
+		}
+		if newSpan.min < rng.inputMin {
+			if newSpan.max < rng.inputMin {
+				res = append(res, newSpan)
+				break
+			}
+			res = append(res, Span{newSpan.min, rng.inputMin - 1})
+			newSpan.min = rng.inputMin
+		}
+		if newSpan.min >= rng.inputMin {
+			if newSpan.max <= rng.inputMax {
+				res = append(res, Span{rng.outputMin + (newSpan.min - rng.inputMin), rng.outputMin + (newSpan.max - rng.inputMin)})
+				break
+			}
+			res = append(res, Span{rng.outputMin + (newSpan.min - rng.inputMin), rng.outputMin + (rng.inputMax - rng.inputMin)})
+			newSpan.min = rng.inputMax + 1
+		}
 	}
-	return -1
+	return res
 }
 
 type CustomMap struct {
@@ -64,28 +98,16 @@ func parseInput(input []string) ([]int, []CustomMap) {
 			res0, _ := strconv.Atoi(lineSplit[0])
 			res1, _ := strconv.Atoi(lineSplit[1])
 			res2, _ := strconv.Atoi(lineSplit[2])
-			newRange := CustomRange{res0, res1, res2}
+			newRange := CustomRange{res0, res1, res1 + res2 - 1}
 
 			new.ranges = append(new.ranges, newRange)
 		}
+		sort.Slice(new.ranges, func(i int, j int) bool {
+			return new.ranges[i].inputMin < new.ranges[j].inputMin
+		})
 		maps = append(maps, new)
 	}
-
 	return seeds, maps
-}
-
-func solveSeedLocation(seed int, maps []CustomMap) int {
-	nextInput := seed
-	for _, cMap := range maps {
-		for _, cRange := range cMap.ranges {
-			if out := cRange.getOutput(nextInput); out != -1 {
-				nextInput = out
-				// found = true
-				break
-			}
-		}
-	}
-	return nextInput
 }
 
 func main() {
@@ -95,7 +117,7 @@ func main() {
 	inputs := common.ReadInput(inputFlag)
 	seeds, maps := parseInput(inputs)
 
-	res := make(chan int, 1000)
+	res := make(chan int, len(seeds)/2)
 	var wg sync.WaitGroup
 
 	for idx, s := range seeds {
@@ -103,10 +125,19 @@ func main() {
 			wg.Add(1)
 			go func(idx int, s int) {
 				defer wg.Done()
-				fmt.Printf("starting hunt for seed %d\n", s)
+				currSpans := []Span{Span{s, s + seeds[idx+1] - 1}}
+				// fmt.Printf("starting hunt for seed range [%+v]\n", sp)
+
+				for _, m := range maps {
+					newSpans := make([]Span, 0)
+					for _, cp := range currSpans {
+						newSpans = append(newSpans, calcSpans(cp, m)...)
+					}
+					currSpans = newSpans
+				}
 				locationMin := math.MaxInt
-				for s2 := s; s2 < s+seeds[idx+1]; s2++ {
-					locationMin = min(solveSeedLocation(s2, maps), locationMin)
+				for _, sp := range currSpans {
+					locationMin = min(sp.min, locationMin)
 				}
 				res <- locationMin
 			}(idx, s)
